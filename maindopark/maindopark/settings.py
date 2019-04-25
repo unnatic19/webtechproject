fcb5fd250ae60da308b0bc54fd876b52489576d7
unnatic19 <unnatichaturvedi295@gmail.com>
2019-04-25 19:48:07 +0530

    third commit

--- maindopark/maindopark/settings.py
+++ maindopark/maindopark/settings.py
@@ -124,12 +124,11 @@ LOGOUT_REDIRECT_URL = 'profile_new'
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
-
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, "static"),
-    )
+    os.path.join(BASE_DIR, "shared"),
+   )
